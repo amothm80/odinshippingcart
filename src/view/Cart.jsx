d56f5e9d020a6c2d6b5de4253bb872300728d8ec
Ahmed O <amo.1980@gmail.com>
2025-01-11 07:48:07 +0300

    cart view and modifying quantities

--- src/view/Cart.jsx
+++ src/view/Cart.jsx
@@ -1,16 +1,27 @@
 import styles from "./Cart.module.css";
 import { Link, useOutletContext } from "react-router";
 import { useProduct } from "/src/api/useProduct";
+import { useState } from "react";
 
-function CartEntry({ entry }) {
+function CartEntry({ entry, cartCont }) {
+  const [quantity, setQuantity] = useState(entry[1]);
   const { product, isLoading, error } = useProduct(entry[0]);
+  function onValueChange(e){
+    setQuantity(e.target.value)
+    cartCont.modifyProduct(entry[0],e.target.value)
+  }
   if (error) return <div>Failed to Load...</div>;
   if (isLoading) return <div>loadin...</div>;
+  const subtotal = quantity * product.price;
+
   return (
-    <div>
-      <div>{product.title}</div>
-      <div>{product.price}</div>
-      <div>{entry[1]}</div>
+    <div className={styles.cartLine}>
+      <h3>{product.title}</h3>
+      <input type="number" value={quantity} onChange={onValueChange} />
+      <h3>{product.price}</h3>
+      <h4>subtotal:</h4>
+      <h3>{subtotal}</h3>
+      <button>delete</button>
     </div>
   );
 }
@@ -20,7 +31,7 @@ export default function Cart() {
   const cart = cartCont.getCart();
   return cartCont.getCartLength() > 0 ? (
     Array.from(cart).map((entry) => {
-      return <CartEntry key={entry[0]} entry={entry} />;
+      return <CartEntry key={entry[0]} entry={entry} cartCont={cartCont} />;
     })
   ) : (
     <h1>add some products</h1>
